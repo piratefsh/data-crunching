62041c72e4eaff56de3b9953ed075b2c60a417b6
Sher Minn Chong <sherminn.chong@gmail.com>
2014-08-14 12:04:34 +0800

    Print responses of target group

--- constants.rb
+++ constants.rb
@@ -9,5 +9,6 @@ $keys = {
 	:gender => 'gender',
 	:company => 'company',
 	:programmed => "Have you programmed before?",
-	:programmed_languages => "Which programming languages are you familiar with?"
+	:programmed_languages => "Which programming languages are you familiar with?",
+	:why_attend => "Why do you want to attend this workshop?"
 }
--- filter-data.rb
+++ filter-data.rb
@@ -3,6 +3,11 @@ require_relative 'csv-plus'
 require_relative 'constants'
 require_relative 'rgkl-data-crunch'
 
+# Taken from http://www.java2s.com/Code/Ruby/String/WordwrappingLinesofText.htm
+def wrap(s, width=78)
+  s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
+end
+
 file_name = ARGV[0];
 
 if not file_name
@@ -31,10 +36,11 @@ print_programmed_in_tech(table)
 print_students(table)
 
 #puts 'Companies: ' + table.get_values($keys[:company]).uniq.to_s
-
-target_total = (beginner & female).size
+target= (beginner & female)
+target_total = target.size
 puts "Total female and beginner applicants: %d (%2.2f%%)" % [target_total, target_total * 100.0 / table.size]
 
+puts "Why they want to attend Rails Girls:\n--------------"
 
-
+target.each {|t| puts "%s-----------\n" % wrap(t[$keys[:why_attend]] ||= 'n/a', 100)}
 puts
